212e91c6bcef268ae188eba8919d1fbd33ca5dd6
Rhalza <rhalza.dev@gmail.com>
2026-06-08 07:05:56 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -3,9 +3,9 @@ import * as THREE from 'three';
 export class Player {
     constructor(camera) {
         this.camera = camera;
-        this.position = new THREE.Vector3(0, 0.1, 0);
+        this.position = new THREE.Vector3(0, 0, 0);
         this.velocity = new THREE.Vector3(0, 0, 0);
-        this.radius = 0.3;
+        this.radius = 0.25;
         this.height = 1.8;
         this.yaw = 0;
         this.pitch = 0;
@@ -20,7 +20,7 @@ export class Player {
         );
     }
 
-    update(dt, input, colliders) {
+    update(dt, input, wallColliders) {
         this.yaw -= input.mouse.movementX * 0.002;
         this.pitch -= input.mouse.movementY * 0.002;
         
@@ -44,8 +44,8 @@ export class Player {
                 new THREE.Vector3(this.position.x + this.radius, this.position.y + 1.8, this.position.z + this.radius)
             );
             let blocked = false;
-            for (let i = 0; i < colliders.length; i++) {
-                if (standBox.intersectsBox(colliders[i])) {
+            for (let i = 0; i < wallColliders.length; i++) {
+                if (standBox.intersectsBox(wallColliders[i])) {
                     blocked = true;
                     break;
                 }
@@ -63,52 +63,57 @@ export class Player {
 
         moveDir.applyAxisAngle(new THREE.Vector3(0, 1, 0), this.yaw);
 
-        const speed = targetHeight === 0.8 ? 0.8 : 1.8;
+        const speed = targetHeight === 0.8 ? 1.0 : 2.0;
         
         const dx = moveDir.x * speed * dt;
-        this.position.x += dx;
-        this.updateAABB();
-        for (let i = 0; i < colliders.length; i++) {
-            if (this.aabb.intersectsBox(colliders[i])) {
-                if (dx > 0) this.position.x = colliders[i].min.x - this.radius;
-                else if (dx < 0) this.position.x = colliders[i].max.x + this.radius;
-                this.updateAABB();
+        if (dx !== 0) {
+            this.position.x += dx;
+            this.updateAABB();
+            for (let i = 0; i < wallColliders.length; i++) {
+                if (this.aabb.intersectsBox(wallColliders[i])) {
+                    if (dx > 0) this.position.x = wallColliders[i].min.x - this.radius;
+                    else if (dx < 0) this.position.x = wallColliders[i].max.x + this.radius;
+                    this.updateAABB();
+                }
             }
         }
 
         const dz = moveDir.z * speed * dt;
-        this.position.z += dz;
-        this.updateAABB();
-        for (let i = 0; i < colliders.length; i++) {
-            if (this.aabb.intersectsBox(colliders[i])) {
-                if (dz > 0) this.position.z = colliders[i].min.z - this.radius;
-                else if (dz < 0) this.position.z = colliders[i].max.z + this.radius;
-                this.updateAABB();
+        if (dz !== 0) {
+            this.position.z += dz;
+            this.updateAABB();
+            for (let i = 0; i < wallColliders.length; i++) {
+                if (this.aabb.intersectsBox(wallColliders[i])) {
+                    if (dz > 0) this.position.z = wallColliders[i].min.z - this.radius;
+                    else if (dz < 0) this.position.z = wallColliders[i].max.z + this.radius;
+                    this.updateAABB();
+                }
             }
         }
 
         if (this.isGrounded && input.keys.space) {
-            this.velocity.y = 4;
+            this.velocity.y = 3.5;
         }
 
-        this.velocity.y -= 15 * dt;
+        this.velocity.y -= 12 * dt;
         const dy = this.velocity.y * dt;
+        
         this.position.y += dy;
         this.updateAABB();
         
         this.isGrounded = false;
-        for (let i = 0; i < colliders.length; i++) {
-            if (this.aabb.intersectsBox(colliders[i])) {
-                if (dy < 0) {
-                    this.position.y = colliders[i].max.y;
-                    this.isGrounded = true;
-                    this.velocity.y = 0;
-                } else if (dy > 0) {
-                    this.position.y = colliders[i].min.y - this.height;
-                    this.velocity.y = 0;
-                }
-                this.updateAABB();
-            }
+
+        if (this.position.y <= 0) {
+            this.position.y = 0;
+            this.isGrounded = true;
+            this.velocity.y = 0;
+            this.updateAABB();
+        }
+
+        if (this.position.y + this.height > 2.7) {
+            this.position.y = 2.7 - this.height;
+            this.velocity.y = 0;
+            this.updateAABB();
         }
 
         this.camera.position.set(this.position.x, this.position.y + this.height - 0.15, this.position.z);
